b144621c2cf4232424b4142345e9f8b9bf105200
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-09-19 15:02:41 -0500

    warning errors fixed

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <body className={roboto.className} >
+      <body className={roboto.className} suppressHydrationWarning={true}>
         <Sidebar />
         {children}
       </body>
