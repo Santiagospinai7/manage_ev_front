de2e4370db97a237fe6a55093144d5e1225ca50e
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-09-23 16:27:49 -0500

    change

--- src/app/chargePoint/new/page.jsx
+++ src/app/chargePoint/new/page.jsx
@@ -57,7 +57,7 @@ const ChargingPointForm = () => {
             name="name"
             id="name"
             className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-            placeholder="Ejemplo: Punto de Carga A"
+            placeholder="Ejemplo: Punto de Carga A" 
             required
             onChange={handleChange}
           />
