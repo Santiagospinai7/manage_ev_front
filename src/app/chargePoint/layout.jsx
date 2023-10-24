bf15b93cac925d88cc461830cf8748181281ca45
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-10-23 21:16:16 -0500

    get charge points with  redux  endpoints

--- src/app/chargePoint/layout.jsx
+++ src/app/chargePoint/layout.jsx
@@ -1,5 +1,6 @@
 import Sidebar from '@/components/Sidebar'
 import Navbar from '@/components/Navbar'
+import {ReduxProvider} from '@/redux/provider'
 
 export default function chargePointLayout({ children }) {
   return (
@@ -7,7 +8,7 @@ export default function chargePointLayout({ children }) {
       <Sidebar />
       <div className="flex-1 ml-0 md:ml-48"> {/* Remove ml-48 on mobile */}
         <Navbar />
-        <div className="mt-16 h-full">{children}</div>
+        <div className="mt-16 h-full"><ReduxProvider>{children}</ReduxProvider></div>
       </div>
     </div>
   );
