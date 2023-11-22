9880210950d497859d6cd457c4f2d85d4e37ae05
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-11-22 00:33:17 -0500

    fix frontend stuff

--- src/app/map/page.jsx
+++ src/app/map/page.jsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 import MapContainer from "@/components/MapContainer"
 import DirectionsForm from "@/components/DirectionsForm"
 import RoutePopUp from "@/components/RoutePopUp"
+import { useGetChargePointsQuery } from '@/redux/features/chargePointsSlice';
 
 const Map = () => {
   const [directions, setDirections] = useState(null);
@@ -56,6 +57,8 @@ const Map = () => {
     setDirections(null);
   };
 
+  const { data: chargePoints, isSuccess } = useGetChargePointsQuery();
+
   useEffect(() => {
     handleGeolocation();
   }, []);
@@ -69,22 +72,81 @@ const Map = () => {
     };
   }, []);
 
+  // useEffect(() => {
+  //   if (selectedRoute) {
+  //     console.log('Selected Route:', selectedRoute);
+  //     const polyline = selectedRoute.informacion_ruta.overview_polyline;
+  //     const arrayPath = google.maps.geometry.encoding.decodePath(polyline);
+
+  //     const directionsService = new window.google.maps.DirectionsService();
+  //     directionsService.route(
+  //       {
+  //         origin: arrayPath[0],
+  //         destination: arrayPath[arrayPath.length - 1],
+  //         provideRouteAlternatives: true,
+  //         travelMode: 'DRIVING',
+  //       },
+  //       (response, status) => {
+  //         if (status === 'OK') {
+  //           response.routes = response.routes.filter((route) => route.overview_polyline === polyline); 
+  //           setDirections(response);
+  //         } else {
+  //           console.error('Error getting directions:', status);
+  //         }
+  //       }
+  //     );
+  //   }
+  // }, [selectedRoute]);
+
   useEffect(() => {
-    if (selectedRoute) {
+    if (selectedRoute && isSuccess) {
       const polyline = selectedRoute.informacion_ruta.overview_polyline;
       const arrayPath = google.maps.geometry.encoding.decodePath(polyline);
-
+      
+      // Assuming you have the array of charge points in `chargePoints`
+      console.log('Charge Points:', chargePoints);
+  
+      // Calculate distances to find the nearest charge point
+      const distances = chargePoints.map(chargePoint => {
+        const distance = google.maps.geometry.spherical.computeDistanceBetween(
+          new google.maps.LatLng(arrayPath[0].lat(), arrayPath[0].lng()),
+          new google.maps.LatLng(chargePoint.latitude, chargePoint.longitude)
+        );
+        return distance;
+      });
+  
+      // Find the index of the charge point with the minimum distance
+      const minDistanceIndex = distances.indexOf(Math.min(...distances));
+  
+      // Get the address of the nearest charge point
+      const nearestChargePointAddress = chargePoints[minDistanceIndex].address;
+      console.log('Nearest Charge Point Address:', nearestChargePointAddress);
+  
       const directionsService = new window.google.maps.DirectionsService();
+      const waypoints = [
+        {
+          location: nearestChargePointAddress,
+          stopover: true,
+        },
+      ];
+  
       directionsService.route(
         {
           origin: arrayPath[0],
           destination: arrayPath[arrayPath.length - 1],
+          waypoints: selectedRoute.necesita_recargar ? waypoints : [],
           provideRouteAlternatives: true,
           travelMode: 'DRIVING',
         },
         (response, status) => {
           if (status === 'OK') {
-            response.routes = response.routes.filter((route) => route.overview_polyline === polyline); 
+            console.log('Directions before:', response);
+            if (selectedRoute.necesita_recargar) {
+              // Remove the first waypoint because it is the nearest charge point
+              response.routes.forEach((route) => route.legs[0].steps.shift());
+            } else {
+              response.routes = response.routes.filter((route) => route.overview_polyline === polyline);
+            }
             setDirections(response);
           } else {
             console.error('Error getting directions:', status);
@@ -92,7 +154,8 @@ const Map = () => {
         }
       );
     }
-  }, [selectedRoute]);
+  }, [selectedRoute, chargePoints]);
+  
 
   return (
     <div className="flex h-full overscroll-none">
